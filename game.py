78820b8779c78fe928231c5915bfc1e29edb0a66
greenpanda-lol <180117139+greenpanda-lol@users.noreply.github.com>
2025-06-11 19:12:51 +0200

    alapok

---
+++ game.py
@@ -0,0 +1,26 @@
+# alap változók
+
+dollar = 0
+day = 1
+level = 1
+# még ide írd azt ami fontos
+
+# ez csak nekem lesz gyorsabb xd
+def a(text):
+    print(text)
+def b():
+    print()
+
+# menü
+def menu():
+    a("ShopGame")
+    b()
+    a(f"» PÉNZ: {dollar}$")
+    a(f"» NAP: {day}.")
+    a(f"» SZINT: {level}.")
+    b()
+    a("FEJLESZTÉSEK|  +NAP  |BEFEKTETÉS")
+    a("      |     |    |   |     |    ")
+    a("      f     |    n   |     b    ")
+
+menu()
