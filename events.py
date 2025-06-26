670d776b83f35d14b52548e6ca18be1afa974c89
Dani <vargadanix@gmail.com>
2025-06-26 21:27:26 +0200

    kezdetleges eventek, az Eventeffect metódus nem működik (de kapott egy szép átnevezést)

--- events.py
+++ events.py
@@ -1,31 +1,48 @@
 import random
 import game
+
 # összes esemény
+
 events = {
  "rablas": {
     "id": "r",
-     "moneyeffect": -500
-
-
+     "moneyeffect": -500,
+     "text": "Az éjjel kirabolták a boltodat! 500 Dollárt vittek el."
  },
 
- "kisrablas":{
+ "kisrablas": {
      "id": "kr",
-     "moneyeffect": -500
+     "moneyeffect": -200,
+     "text": "Az éjjel kirabolták a boltodat! 500 Dollárt vittek el."
+ },
 
+ "palyazat": {
+    "id": "p",
+     "moneyeffect": 500,
+     "text": "A boltod megnyert egy pályázatot! A pénzdíj 500 Dollár, és kaptatok egy menő oklevelet"
+ },
+ "adomanyok": {
+    "id": "p",
+    "moneyeffect": 200,
+    "text": "A boltod kisebb, nagyobb adományokból egészen sok pénzt gyűjtött össze!"
  }
+
 }
-def eventchoice():
+
+
+def eventChoice():
     event = random.choice(list(events.items()))
     return event
 # választ egy eventet
 
-def effect():
+def eventEffect():
     game.dollar += event["moneyeffect"]
-    print (f' A pénzed: {game.dollar}')
+    print(f'{event["text"]}')
 # végrehajtja az event hatását
 # nem műkszik :(
 
-
+def eventHappen():
+    eventChoice()
+    eventEffect()
 
 
